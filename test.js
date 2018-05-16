18535e2eb33255212318bb594902ea97edbc29a5
Rishi Giri <rushforlinux@gmail.com>
2018-05-16 14:36:15 +0530

    passed

--- test.js
+++ test.js
@@ -1,25 +1,32 @@
 import test from 'ava';
 import fn from './';
 
-test('goodreads', async t => {
-	const kote = await fn.goodReads();
-
-	t.is(kote.quote,
-		'“O, ah! The awareness of emptiness brings forth a heart of compassion!”'
-	);
-});
-
 test('brainyquote', async t => {
 	const kote = await fn.brainyQuote();
+	t.is(kote.quote, 'Words are often seen hunting for an idea, but ideas are never seen hunting for words.');
+});
 
-	t.is(kote.quote,
-		`Be pleasant until ten o'clock in the morning and the rest of the day will take care of itself.`
-	);
+test('brainyquoteLove', async t => {
+	const kote = await fn.brainyQuote('love');
+	t.is(kote.quote, 'Love is an energy which exists of itself. It is its own value.');
+});
+
+test('brainyquoteNature', async t => {
+	const kote = await fn.brainyQuote('nature');
+	t.is(kote.quote, 'A good garden may have some weeds.');
+});
+
+test('brainyquoteArt', async t => {
+	const kote = await fn.brainyQuote('art');
+	t.is(kote.quote, 'Anything simple always interests me.');
+});
+
+test('brainyquoteFunny', async t => {
+	const kote = await fn.brainyQuote('funny');
+	t.is(kote.quote, 'He was like a cock who thought the sun had risen to hear him crow.');
 });
 
 test('eduro', async t => {
 	const kote = await fn.eduro();
-
-	t.is(kote.quote,
-		'Opportunity does not knock, it presents itself when you beat down the door.');
+	t.is(kote.quote, 'A good decision is based on knowledge and not on numbers.');
 });
